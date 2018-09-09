617dad756e49e526c72cabec8c18ba00a5e13880
Wouter Roos <wrmmroos@outlook.com>
2018-09-09 16:07:15 +0200

    initial commit

--- AssociateTestsToTestCases/Program.cs
+++ AssociateTestsToTestCases/Program.cs
@@ -1,26 +1,93 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using CommandLine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minimatch;
 
 namespace AssociateTestsToTestCases
 {
-    class Program
+    internal class Program
     {
-        public class Options
-        {
-            [Option('f', "test files", Required = true, HelpText = "Supports multiple minimatch patterns, separated by a semicolon.")]
-            public string TestFiles { get; set; }
-        }
-
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Parser.Default.ParseArguments<Options>(args)
-            .WithParsed<Options>(o =>
+            .WithParsed(o =>
             {
-                var minimatchPatterns = o.TestFiles.Split(';');
+                var testAssemblyPaths = LocateTestAssemblies(o.Directory, o.MinimatchPatterns.Split(';'));
+                var testMethods = ListTestMethods(testAssemblyPaths);
+
+                //Todo: retrieve, match and associate test cases from VSTS with test methods.
 
             });
 
             Console.WriteLine("Hello World!");
+        }
+
+        private static MethodInfo[] ListTestMethods(string[] testAssemblyPaths)
+        {
+            var testMethods = new List<MethodInfo>();
+
+            foreach (var testAssemblyPath in testAssemblyPaths)
+            {
+                var testAssembly = Assembly.LoadFrom(testAssemblyPath);
+                testMethods.AddRange(testAssembly.GetTypes().SelectMany(type => type.GetMethods().Where(method => method.GetCustomAttribute<TestMethodAttribute>() != null)));
+            }
+
+            return testMethods.ToArray();
+        }
+
+        private static string[] LocateTestAssemblies(string directory, string[] minimatchPatterns)
+        {
+            var files = ListAllAccessibleFilesInDirectory(directory);
+            var matchingFilesToBeIncluded = new List<string>();
+
+            foreach (var minimatchPattern in minimatchPatterns.Where(minimatchPattern => !minimatchPattern.StartsWith("!")))
+            {
+                var mm = new Minimatcher(minimatchPattern, new Minimatch.Options() { AllowWindowsPaths = true });
+                matchingFilesToBeIncluded.AddRange(mm.Filter(files));
+            }
+
+            var matchingFilesToBeExcluded = new List<string>();
+            foreach (var minimatchPattern in minimatchPatterns.Where(minimatchPattern => minimatchPattern.StartsWith("!")))
+            {
+                var actualMinimatchPattern = minimatchPattern.TrimStart('!');
+                var mm = new Minimatcher(actualMinimatchPattern, new Minimatch.Options() { AllowWindowsPaths = true });
+                matchingFilesToBeExcluded.AddRange(mm.Filter(files));
+            }
+
+            matchingFilesToBeIncluded.RemoveAll(x => matchingFilesToBeExcluded.Contains(x));
+
+            return matchingFilesToBeIncluded.ToArray();
+        }
+
+        private static string[] ListAllAccessibleFilesInDirectory(string directory)
+        {
+            var files = new List<string>(Directory.GetFiles(directory, "*.*", SearchOption.TopDirectoryOnly));
+            foreach (var subDir in Directory.GetDirectories(directory))
+            {
+                try
+                {
+                    files.AddRange(ListAllAccessibleFilesInDirectory(subDir));
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    //Ignored by design, see https://stackoverflow.com/a/19137152. Comment is here to suppress analyzer warnings.
+                }
+            }
+
+            return files.ToArray();
+        }
+
+        private class Options
+        {
+            [Option('d', "directory", Required = true, HelpText = "The root directory to search")]
+            public string Directory { get; set; }
+
+            [Option('m', "minimatch patterns", Required = true, HelpText = "Supports multiple minimatch patterns, separated by a semicolon.")]
+            public string MinimatchPatterns { get; set; }
         }
     }
 }
