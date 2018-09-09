7eaf7bfe4bb0e8131590d8082e300b73db54a7ef
Wouter Roos <wrmmroos@outlook.com>
2018-09-09 15:27:58 +0200

    Add project files.

---
+++ AssociateTestsToTestCases/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CommandLine;
+
+namespace AssociateTestsToTestCases
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('f', "test files", Required = true, HelpText = "Supports multiple minimatch patterns, separated by a semicolon.")]
+            public string TestFiles { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+            .WithParsed<Options>(o =>
+            {
+                var minimatchPatterns = o.TestFiles.Split(';');
+
+            });
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
