0652d1799ed32ca9f6ad49942816a6230eff53e3
Tawwab Djalielie <tawwab.djalielie@qnh.nl>
2018-11-12 14:59:30 +0100

    Added: colors!

--- AssociateTestsToTestCases/Access/Output/CommandLineAccess.cs
+++ AssociateTestsToTestCases/Access/Output/CommandLineAccess.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using AssociateTestsToTestCases.Message;
+using AssociateTestsToTestCases.Message.Type;
 
 namespace AssociateTestsToTestCases.Access.Output
 {
@@ -20,7 +21,29 @@ namespace AssociateTestsToTestCases.Access.Output
             var reasonOutputFormat = reason.Length.Equals(0) ? string.Empty : string.Format(Format, reason);
             var spaceReason = reason.Length.Equals(0) ? string.Empty : new string(' ', messages.Reasons.LongestReasonCount - reason.Count() + 1);
 
+            Console.ForegroundColor = GetConsoleColor(messages.Types, messageType);
             Console.WriteLine(OutputMessage, indention, messageTypeFormat, spaceMessageType, reasonOutputFormat, spaceReason, message);
+        }
+
+        private static ConsoleColor GetConsoleColor(TypeMessage types, string messageType)
+        {
+            var consoleColor = ConsoleColor.DarkCyan;
+
+            if (messageType.Equals(types.Success))
+            {
+                consoleColor = ConsoleColor.DarkGreen;
+            } else if (messageType.Equals(types.Warning))
+            {
+                consoleColor = ConsoleColor.DarkYellow;
+            } else if (messageType.Equals(types.Error) || messageType.Equals(types.Failure))
+            {
+                consoleColor = ConsoleColor.DarkRed;
+            }  else if (messageType.Equals(types.Stage))
+            {
+                consoleColor = ConsoleColor.Gray;
+            }
+
+            return consoleColor;
         }
     }
 }
--- AssociateTestsToTestCases/Message/Stage/AssociationMessage.cs
+++ AssociateTestsToTestCases/Message/Stage/AssociationMessage.cs
@@ -4,6 +4,6 @@
     {
         public readonly string Status = "Trying to Associate Test Cases with Test Methods...";
         public readonly string Success = "Test Cases and Test Methods have been associated ({0}).\n";
-        public readonly string Failure = "Could not associate every Test Method with its corresponding Test Case ({0}):";
+        public readonly string Failure = "Could not associate the following Test Methods with its corresponding Test Cases ({0}):";
     }
 }
