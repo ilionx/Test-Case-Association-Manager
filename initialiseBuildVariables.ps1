3ef6a79bab2131a6f5b16a4f68759bdc7b77d11f
Tawwab Djalielie <tawwab.djalielie@qnh.nl>
2018-12-07 13:06:33 +0100

    Fix: script - execution order

--- initialiseBuildVariables.ps1
+++ initialiseBuildVariables.ps1
@@ -1,9 +1,9 @@
 ﻿Write-Host "Retrieving build variables..."
-[String]$versionNumberPatch = $buildNumber.Substring($buildNumber.LastIndexOf('.') + 1)
 [String]$buildNumber = $Env:BUILD_BUILDNUMBER
+[String]$versionNumberPatch = $buildNumber.Substring($buildNumber.LastIndexOf('.') + 1)
 Write-Host "Success! Build variables have been retrieved."
 
 Write-Host "Initialising build variables..."
-Write-Host "##vso[task.setvariable variable=VersionNumberPatch;]$($versionNumberPatch)"
 Write-Host "##vso[task.setvariable variable=VersionNumberFull;]$($buildNumber)"
+Write-Host "##vso[task.setvariable variable=VersionNumberPatch;]$($versionNumberPatch)"
 Write-Host "Success! Build variables have been initialised."
