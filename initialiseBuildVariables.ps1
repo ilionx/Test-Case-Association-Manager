91bb1f10d340df7846784775414a3ad96d6c6b20
Tawwab Djalielie <51912376+tawwabDjalielie@users.noreply.github.com>
2020-01-14 13:35:40 +0100

    fixed version number full (#9)

--- initialiseBuildVariables.ps1
+++ initialiseBuildVariables.ps1
@@ -1,9 +1,13 @@
 ﻿Write-Host "Retrieving build variables..."
+$Env:BUILD_BUILDNUMBER
+[String]$versionNumberMajor = $Env:VersionNumberMajor
+[String]$versionNumberMinor = $Env:VersionNumberMinor
 [String]$buildNumber = $Env:BUILD_BUILDNUMBER
 [String]$versionNumberPatch = $buildNumber.Substring($buildNumber.LastIndexOf('.') + 1)
+[String]$versionNumberFull =  "$($versionNumberMajor).$($versionNumberMinor).$($versionNumberPatch)"
 Write-Host "Success! Build variables have been retrieved."
 
 Write-Host "Initialising build variables..."
-Write-Host "##vso[task.setvariable variable=VersionNumberFull;]$($buildNumber)"
 Write-Host "##vso[task.setvariable variable=VersionNumberPatch;]$($versionNumberPatch)"
+Write-Host "##vso[task.setvariable variable=VersionNumberFull;]$($versionNumberFull)"
 Write-Host "Success! Build variables have been initialised."
